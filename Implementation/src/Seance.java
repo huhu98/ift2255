513c8fbc4424c076017e7bfd4d9d32e8f75a4145
maxime <max_lech_13@outlook.com>
2020-08-02 14:26:00 -0400

    avancement Sceance Class

--- Implementation/src/Seance.java
+++ Implementation/src/Seance.java
@@ -1,10 +1,48 @@
 public class Seance {
-    private Professionnel pro;
-    private int heure;
+
     private String date;
     private Services services;
+    private String codeSeance;
+    private String[] inscriptions = new String[30];
+    private String[] presences = new String[30];
 
-    public Seance(){
+    public void Seance(Services services, String date, String codeSeance, String[] inscriptions, String[] presences) {
+        this.date = date;
+        this.codeSeance = codeSeance;
+        this.presences = presences;
+        this.inscriptions = inscriptions;
+        this.services = services;
 
+    }
+    public void ajoutPresence(String numM) {
+        for (int i = 0; i <= presences.length; i++) {
+            presences[i] = numM;
+        }
+    }
+
+    public void ajoutInscrit(String numM) {
+        for (int i = 0; i <= inscriptions.length; i++) {
+            inscriptions[i] = numM;
+        }
+    }
+    public String getContenu(String champ) {
+        String value = "";
+        switch (champ) {
+            case "presences":
+                value = presences.toString();
+                break;
+            case "inscriptions":
+                value = inscriptions.toString();
+                break;
+            case "date":
+                value = date;
+                break;
+            case "codeSeance":
+                value = codeSeance;
+                break;
+            case "services":
+                value = services.toString();
+        }
+        return value;
     }
 }
